2c8c9af733ba92fefad246cf359ef2c7499dbf7f
Ibrahim <semarcapada@gmail.com>
2019-11-12 19:52:18 +0700

    Updates

---
+++ precache-manifest.4f9fec5e414707d1d353bd4b11b1ec9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b5086246dc2404e481558ed899b1dab",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "46ff95e67cd518e96edf",
+    "url": "/robofriends/static/css/main.6dd4b4ef.chunk.css"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriends/static/js/2.2a21f297.chunk.js"
+  },
+  {
+    "revision": "46ff95e67cd518e96edf",
+    "url": "/robofriends/static/js/main.26bc955c.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
